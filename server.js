1f6518ecabd7659cae4474959e037b6b75635e2b
shantanu tripathi <shantanu.akt@gmail.com>
2018-05-03 21:34:11 +0530

    added server.js and package.json

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+app.use("/styles", express.static(__dirname));
+app.use("/images", express.static(__dirname + '/images'));
+app.use("/scripts", express.static(__dirname + '/scripts'));
+
+// viewed at based directory http://localhost:8080/
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + 'index.html'));
+});
+
+// add other routes below
+const PORT = (process.env.PORT || 8080);
+app.listen(PORT);
+console.log("Server started at port " + PORT);
